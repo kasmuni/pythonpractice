50b0f4e754f14924d3d363e0f36738b8dea97fbe
kasmuni <kasmuni@gmail.com>
2021-05-31 21:31:05 -0400

    added unit tests for fruits class

--- tests/fruits_test.py
+++ tests/fruits_test.py
@@ -3,12 +3,18 @@ from exercisefunctions import fruits
 
 # The unit tests
 @pytest.fixture
-def setup():
+def apple():
     return fruits.fruit("Apple","Green","Large")
 
-def test_get_fruit_name(setup):
-    assert setup.get_fruit_name() == "Apple"
+def test_get_fruit_name(apple):
+    assert apple.get_fruit_name() == "Apple"
 
-def test_get_fruit_color(setup):
-    assert setup.get_fruit_color() == "Green"
+def test_get_fruit_color(apple):
+    assert apple.get_fruit_color() == "Green"
 
+def test_get_fruit_size(apple):
+    assert apple.get_fruit_size() == "Large"
+
+def test_bite_percentage(apple):
+    apple.bite()
+    assert apple.get_balance() == 90
